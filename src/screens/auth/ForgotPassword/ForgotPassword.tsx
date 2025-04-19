5ec255e73c49a5201476b028666a0926ff4e0576
MBlml <juan.barajas5570@alumnos.udg.mx>
2025-04-19 00:00:09 -0600

    ForgotPassword new styles

--- src/screens/auth/ForgotPassword/ForgotPassword.tsx
+++ src/screens/auth/ForgotPassword/ForgotPassword.tsx
@@ -1,8 +1,10 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React, { useState } from 'react';
+import { styles } from './styles';
 import {
   StyleSheet,
   Text,
+  Image,
   View,
   TextInput,
   TouchableOpacity
@@ -27,10 +29,8 @@ const ForgotPasswordScreen: React.FC<ForgotPasswordScreenProps> = ({ navigation,
 
   return (
     <View style={styles.container}>
-      <View style={styles.btnContainer}>
-        <TouchableOpacity style={styles.btn} onPress={returnPage}>
-          <Icon name="navigate-before" color="white" size={30} />
-        </TouchableOpacity>
+      <View style={styles.headerImg}>
+        <Image style={styles.image} source={require('../../../assets/logo.png')} />
       </View>
 
       <View style={styles.header}>
@@ -42,16 +42,15 @@ const ForgotPasswordScreen: React.FC<ForgotPasswordScreenProps> = ({ navigation,
 
       <View style={styles.iconContainer}>
         <Icon 
-          name="send" 
+          name="lock-open" 
           color="#200606" 
           size={150} 
-          style={{ transform: [{ rotate: '-45deg' }] }} 
         />
       </View>
 
       <View style={styles.form}>
         <View style={styles.inputLabel}>
-          <Icon name="email" color="#200606" size={30} />
+          <Icon name="email" color="#200606" size={35} />
           <TextInput
             style={styles.input}
             placeholder="Correo"
@@ -69,86 +68,5 @@ const ForgotPasswordScreen: React.FC<ForgotPasswordScreenProps> = ({ navigation,
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F6EDE1',
-  },
-  header: {
-    padding: 20,
-    marginTop: 20,
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-  },
-  subtitle: {
-    padding: 18,
-    fontSize: 22,
-    textAlign: 'center',
-  },
-  form: {
-    padding: 20,
-    alignItems: 'center',
-  },
-  input: {
-    borderWidth: 1,
-    color: 'black',
-    backgroundColor: 'transparent',
-    borderBottomColor: 'black',
-    borderTopWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderBottomWidth: 2,
-    padding: 5,
-    margin: 10,
-    fontSize: 22,
-    width: '70%',
-  },
-  button: {
-    backgroundColor: '#200707',
-    padding: 10,
-    borderRadius: 50,
-    alignItems: 'center',
-    marginTop: 50,
-    marginBottom: 50,
-    width: '80%',
-    shadowColor: '#200606',
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 10,
-    elevation: 16,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#FFF',
-  },
-  inputLabel: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  btnContainer: {
-    width: '100%',
-    alignItems: 'flex-start',
-    marginTop: 50,
-  },
-  btn: {
-    backgroundColor: '#200707',
-    borderRadius: 50,
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-    marginLeft: 20,
-    alignItems: 'center',
-  },
-  iconContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default ForgotPasswordScreen;
