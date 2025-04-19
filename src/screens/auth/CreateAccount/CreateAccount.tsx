6652fb622921b9a0dc19f170b6243c24485dd8b3
MBlml <juan.barajas5570@alumnos.udg.mx>
2025-04-18 23:43:30 -0600

    CreateAccount new styles

--- src/screens/auth/CreateAccount/CreateAccount.tsx
+++ src/screens/auth/CreateAccount/CreateAccount.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import { styles } from './styles';
 import {
   StyleSheet,
   Text,
+  Image,
   View,
   TextInput,
   TouchableOpacity,
@@ -51,10 +53,9 @@ const CreateAccountScreen: React.FC<CreateAccountScreenProps> = ({ navigation, r
 
   return (
     <View style={styles.container}>
-      <View style={styles.btnContainer}>
-        <TouchableOpacity style={styles.btn} onPress={returnPage}>
-          <Icon name="navigate-before" color="white" size={30} />
-        </TouchableOpacity>
+
+      <View style={styles.headerImg}>
+        <Image style={styles.image} source={require('../../../assets/logo.png')} />
       </View>
 
       <View style={styles.header}>
@@ -63,7 +64,7 @@ const CreateAccountScreen: React.FC<CreateAccountScreenProps> = ({ navigation, r
 
       <View style={styles.form}>
         <View style={styles.inputLabel}>
-          <Icon name="person" color="#200606" size={30} />
+          <Icon name="person" color="#200606" size={35} />
           <TextInput
             style={styles.input}
             placeholder="Nombre(s)"
@@ -74,7 +75,7 @@ const CreateAccountScreen: React.FC<CreateAccountScreenProps> = ({ navigation, r
         </View>
 
         <View style={styles.inputLabel}>
-          <Icon name="looks-one" color="#200606" size={30} />
+          <Icon name="looks-one" color="#200606" size={35} />
           <TextInput
             style={styles.input}
             placeholder="Primer apellido"
@@ -85,7 +86,7 @@ const CreateAccountScreen: React.FC<CreateAccountScreenProps> = ({ navigation, r
         </View>
 
         <View style={styles.inputLabel}>
-          <Icon name="looks-two" color="#200606" size={30} />
+          <Icon name="looks-two" color="#200606" size={35} />
           <TextInput
             style={styles.input}
             placeholder="Segundo apellido"
@@ -96,7 +97,7 @@ const CreateAccountScreen: React.FC<CreateAccountScreenProps> = ({ navigation, r
         </View>
 
         <View style={styles.inputLabel}>
-          <Icon name="email" color="#200606" size={30} />
+          <Icon name="email" color="#200606" size={35} />
           <TextInput
             style={styles.input}
             placeholder="Email"
@@ -107,7 +108,7 @@ const CreateAccountScreen: React.FC<CreateAccountScreenProps> = ({ navigation, r
         </View>
 
         <View style={styles.inputLabel}>
-          <Icon name="lock" color="#200606" size={30} />
+          <Icon name="lock" color="#200606" size={35} />
           <TextInput
             style={styles.input}
             placeholder="Contraseña"
@@ -118,114 +119,21 @@ const CreateAccountScreen: React.FC<CreateAccountScreenProps> = ({ navigation, r
           />
         </View>
 
-        <TouchableOpacity style={styles.button} onPress={createAccount}>
-          <Text style={styles.buttonText}>Siguiente</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View>
-        <Text style={styles.txt}>Ya tengo cuenta</Text>
-        <TouchableOpacity
-          style={styles.createAccount}
-          onPress={() => returnPage()}
-        >
-          <Text style={styles.loginText}>Iniciar Sesión</Text>
-        </TouchableOpacity>
+        <View style={styles.containerBottom}>
+          <TouchableOpacity style={styles.button} onPress={createAccount}>
+            <Text style={styles.buttonText}>Siguiente</Text>
+          </TouchableOpacity>
+          <Text style={styles.txt}>¡Ya tengo cuenta!</Text>
+          <TouchableOpacity
+            style={styles.createAccount}
+            onPress={() => returnPage()}
+          >
+            <Text style={styles.loginText}>Iniciar Sesión</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    backgroundColor: '#fbf2e2',
-    padding:20,
-  },
-  header: {
-    padding: 10,
-    marginTop: 10,
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: 'bold',
-  },
-  form: {
-    padding: 20,
-    alignItems: 'center',
-  },
-  input: {
-    borderWidth: 1,
-    color: 'black',
-    backgroundColor: 'transparent',
-    borderBottomColor: 'black',
-    borderTopWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderBottomWidth: 2,
-    padding: 5,
-    margin: 10,
-    marginVertical: 10,
-    fontSize: 22,
-    width: '70%',
-  },
-  button: {
-    backgroundColor: '#200707',
-    padding: 5,
-    borderRadius: 50,
-    alignItems: 'center',
-    marginTop: 30,
-    width: '80%',
-    shadowColor: '#200606',
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 10,
-    elevation: 16,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#FFF',
-  },
-  inputLabel: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  createAccount: {
-    padding: 20,
-    alignItems: 'center',
-  },
-  loginText: {
-    backgroundColor:'#ffffff',
-    fontSize: 22,
-    color: '#763F0F',
-    fontWeight: 'bold',
-    marginTop: -15,
-  },
-  txt: {
-    color: '#200606',
-    fontSize: 20,
-    paddingTop: 15,
-    textAlign: 'center',
-  },
-  btnContainer: {
-    width: '100%',
-    alignItems: 'flex-start',
-    paddingRight: 1,
-    marginTop: 10,
-  },
-  btn: {
-    backgroundColor: '#200707',
-    borderRadius: 50,
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-    marginLeft: 20,
-    alignItems: 'center',
-  },
-});
 
 export default CreateAccountScreen;
